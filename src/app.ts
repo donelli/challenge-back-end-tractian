3d73c0b1cad0a2e73b98825dd4eb1e101387689f
donelli <49295044+donelli@users.noreply.github.com>
2022-01-28 13:08:35 -0300

    install and configure dotnev

--- src/app.ts
+++ src/app.ts
@@ -1,80 +1,23 @@
 
 import * as express from 'express'
-import * as mongoose from 'mongoose'
-
-import { CompanyModel } from './models/company'
-import { UserModel } from './models/user'
+import { initMongoose } from './config/db';
 import { companyRouter } from './routes/company'
+
+// TODO uggly error showing on invalid JSON input 
 
 const app = express()
 app.use(express.json());
 
-// TODO uggly error showing on invalid JSON input 
+initMongoose()
+.then(() => {
 
-const PORT = 8080
-
-// TODO place the database logic in a separate file
-// TODO load this from .env file
-const mongoDbUrl = 'mongodb://localhost:27017/test'
-
-mongoose.connect(mongoDbUrl, {})
-.then(async db => {
-
-   // const model = new CompanyModel({
-   //    name: 'Teste',
-   //    users: [
-   //       new UserModel({
-   //          name: "donelli"
-   //       })
-   //    ],
-   //    units: []
-   // });
-
-   // await model.save();
-   
-   // const model = await CompanyModel.findOne({})
-   
-   // model.users.push(new UserModel({
-   //    'name': 'william'
-   // }));
-   // await model.save();
-
-   // const newUser = new UserModel({
-   //    name: 'New user'
-   // });
-   
-   // const resp = await newUser.save();
-
-   // const id = resp._id;
-   
-   // const newCompany = new CompanyModel({
-   //    name: 'New company',
-   //    users: [],
-   //    units: []
-   // });
-
-   // await newCompany.save();
-   
-   // const companyModel = await CompanyModel.findOne({})
-   // console.log(companyModel);
-   
-   // const userModel = new UserModel({
-   //    name: "Donelli"
-   // })
-
-   // companyModel.users.push(userModel);
-
-   // companyModel.save();
+   const PORT = process.env.PORT
    
    app.use('/api/company', companyRouter);
-
-   
-   // app.get('/', (req: express.Request, res: express.Response) => {
-   //    res.setHeader('Content-Type', 'text/html')
-   //    res.end('<h1>Hello World</h1>')
-   // })
 
    app.listen(PORT, () => console.log(`app running on port ${PORT}`))
    
 })
-.catch(err => console.log(err))
+.catch(err => {
+   console.error(err);
+});
